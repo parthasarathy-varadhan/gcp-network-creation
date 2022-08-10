a8f9e4c3955e388dd445528023d22a91d67eabcf
Parthasarathy Varadhan <parthasarathy.varadhan@peerislands.io>
2022-08-10 15:12:27 -0500

    name change

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "google_compute_network" "default" {
 # Create a Google Sub Network
 resource "google_compute_subnetwork" "default" {
   project       = google_compute_network.default.project
-  name          = var.SUBNET_NAME
+  name          = var.GOOGLE_SUBNET_NAME
   ip_cidr_range = "10.0.0.0/16"
   region        = var.GCP_REGION_NAME
   network       = google_compute_network.default.id
